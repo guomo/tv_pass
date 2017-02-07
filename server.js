9a87a517778fc68859117d153377f613a5d84421
Gregory Stone <guomo@yahoo.com>
2017-02-07 08:05:47 +0000

    Working server with all APIs & Docs

--- server.js
+++ server.js
@@ -5,6 +5,8 @@
 //
 var http = require('http');
 var path = require('path');
+var assert = require('assert');
+var fs = require("fs")
 
 var async = require('async');
 var socketio = require('socket.io');
@@ -21,6 +23,7 @@ var server = http.createServer(router);
 var io = socketio.listen(server);
 
 router.use(express.static(path.resolve(__dirname, 'client')));
+router.use(express.bodyParser());
 var messages = [];
 var sockets = [];
 
@@ -78,15 +81,63 @@ function broadcast(event, data) {
   });
 }
 
-
+// Load up the embedded mongo
+var Engine = require('tingodb')();
+var db = new Engine.Db('./database', {});
+var collection = db.collection("playlist");
 
 router.get('/playlists/', function (req, res) {
   res.send('You must supply an esn, e.g. /playlists/{esn}')
 })
 
+router.get('/playlists/:esn', function (req, res) {
+    
+  collection.findOne({"esn":req.params.esn}, function(err, feed) {
+    assert.equal(null, err);
+    if( null == feed ){
+        res.status(404).send('No playlist found for ' + req.params.esn);
+    }else{
+      res.json(feed)
+    }
+  })
+})
+
+router.put('/playlists/:esn', function (req, res) {
+  
+  // Validate the content
+  if( !checkFeed(req) ){
+    res.status(404).send('No playlist found for ' + req.params.esn);
+  }
+      console.log(req.body)
+
+  // Check if we need to replace 
+  
+    req.body.lastUpdated = new Date();
+    collection.update({esn: req.params.esn, feed: req.body}, {upsert:true, w: 1}, function(err, result) {
+    assert.equal(null, err);
+    res.send("OK")
+    console.log()
+  })
+})
+
+function checkFeed(req){
+  // Check it is JSON content type
+  // Check it parses to actual object
+  return true;
+}
 
 
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
-  var addr = server.address();
-  console.log("Chat server listening at", addr.address + ":" + addr.port);
-});
+
+  server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+    console.log("Creating index...")
+    collection.createIndex( { "esn": 1 }, { "unique" : true }, function(err, results) {
+                        console.log(results);
+                        if(err){
+                          console.log("Error creating index: " + err)
+                        }
+                      }
+                );
+    var addr = server.address();
+    console.log("Playlist server listening at", addr.address + ":" + addr.port);
+  });
+
